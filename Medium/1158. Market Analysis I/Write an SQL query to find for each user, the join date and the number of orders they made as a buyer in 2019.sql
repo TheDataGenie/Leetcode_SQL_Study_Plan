15e3d9f44d68d9a43f88be54626ce9ea6de7ff95
Soma Dey <deysoma94@gmail.com>
2023-03-07 13:13:51 +0530

    Update Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.sql

--- Medium/1158. Market Analysis I/Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.sql
+++ Medium/1158. Market Analysis I/Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.sql
@@ -89,5 +89,19 @@
 -- +-----------+------------+----------------+
 
 
--- Output
+-- Solution
 
+SELECT          u.user_id AS buyer_id
+            ,   u.join_date
+            ,   COUNT(o.order_date) AS orders_in_2019
+
+FROM            users u
+
+LEFT JOIN       orders o
+ON              u.user_id = o.buyer_id
+AND             o.order_date >= '2019-01-01'
+AND             o.order_date < '2020-01-01'
+
+GROUP BY        u.user_id
+            ,   u.join_date
+;
