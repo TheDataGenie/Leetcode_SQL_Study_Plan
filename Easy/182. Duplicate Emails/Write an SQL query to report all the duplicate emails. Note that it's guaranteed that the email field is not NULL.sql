0b32c7a8c14900caf940bcd492bc8556b69bc70e
Soma Dey <deysoma94@gmail.com>
2023-03-08 12:48:15 +0530

    Create Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.sql

---
+++ Easy/182. Duplicate Emails/Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.sql
@@ -0,0 +1,55 @@
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+-- Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Person table:
+-- +----+---------+
+-- | id | email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- Output: 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Explanation: a@b.com is repeated two times.
+
+
+-- Solution
+
+SELECT      DISTINCT
+            a.email AS Email
+
+FROM        (SELECT     email
+                      , COUNT(email) AS email_cnt
+
+             FROM       person
+
+             GROUP BY   email        
+            ) a
+
+WHERE       a.email_cnt > 1
+;
